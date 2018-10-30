47e40b274bb850d96c334a3fa9360b59713fe37a
Lucas m. Roe <lroe@eandrcleaners.local>
2018-10-30 19:27:02 +0000

    Filled in the Dashboard Design View Model

---
+++ WorkoutHelper/Models/WorkoutGroup.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections.Generic;
+
+namespace WorkoutHelper.Models
+{
+    public class WorkoutGroup
+    {
+        public string RepititionType { get; set; }
+
+        public IReadOnlyList<Workout> Workouts { get; set; }
+    }
+}
