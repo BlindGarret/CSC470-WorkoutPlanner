34357f1fa647899bb43869213bd50fee28c3ae97
Lucas m. Roe <lroe@eandrcleaners.local>
2018-10-30 20:01:37 +0000

    Filled in Setting Page

---
+++ WorkoutHelper/DesignViewModels/SettingsDesignViewModel.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections.ObjectModel;
+using Prism.Commands;
+using Prism.Mvvm;
+using WorkoutHelper.Models;
+
+namespace WorkoutHelper.DesignViewModels
+{
+    public class SettingsDesignViewmodel : BindableBase
+    {
+
+        #region Properties
+        
+        public User User { get; set; }
+
+        public WeighIn WeighIn { get; set; }
+
+        #endregion
+
+        #region Commands
+
+        public DelegateCommand SaveCommand { get; set; }
+
+        #endregion
+    }
+}
