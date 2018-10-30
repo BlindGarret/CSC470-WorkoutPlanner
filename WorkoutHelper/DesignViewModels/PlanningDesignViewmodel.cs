8b22c60f8090114495863e7634cfe561c24db2fd
Lucas m. Roe <lroe@eandrcleaners.local>
2018-10-30 19:57:20 +0000

    Filled in the Planning View Model

---
+++ WorkoutHelper/DesignViewModels/PlanningDesignViewmodel.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections.ObjectModel;
+using Prism.Commands;
+using Prism.Mvvm;
+using WorkoutHelper.Models;
+
+namespace WorkoutHelper.DesignViewModels
+{
+    public class PlanningDesignViewmodel : BindableBase
+    {
+
+        #region Properties
+
+        public ObservableCollection<Plan> Plans { get; set; }
+
+        public Plan SelectedPlan { get; set; }
+
+        #endregion
+
+        #region Commands
+
+        public DelegateCommand<WorkoutGroup> AddExercise { get; set; }
+
+        public DelegateCommand AddGroup { get; set; }
+
+        #endregion
+    }
+}
---
+++ WorkoutHelper/Models/PlannedGroup.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WorkoutHelper.Models
+{
+    public class PlannedGroup
+    {
+        public int Order { get; set; }
+
+        public ObservableCollection<PlannedExercise> Exercises { get; set; }
+
+    }
+}
