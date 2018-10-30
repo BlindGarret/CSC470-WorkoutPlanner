eed4fc172563abf7e05742f02925b6b4a0fa49be
Lucas m. Roe <lroe@eandrcleaners.local>
2018-10-30 19:35:36 +0000

    Filled in the Select User View model

---
+++ WorkoutHelper/DesignViewModels/SelectUserViewModel.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections.ObjectModel;
+using Prism.Commands;
+using Prism.Mvvm;
+using WorkoutHelper.Models;
+
+namespace WorkoutHelper.DesignViewModels
+{
+    public class SelectUserViewModel : BindableBase
+    {
+
+        #region Properties
+
+        public ObservableCollection<User> Users { get; set; }
+
+        #endregion
+
+        #region Commands
+        
+        public DelegateCommand<User> SelectUserCommand { get; set; }
+
+        public DelegateCommand<User> AddUserCommand { get; set; }
+
+        #endregion
+    }
+}
