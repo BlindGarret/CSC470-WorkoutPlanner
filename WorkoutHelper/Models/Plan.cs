d80b541e74aa5c7b0f50e3b44a00040a7933c407
Lucas m. Roe <lroe@eandrcleaners.local>
2018-10-30 19:57:20 +0000

    Filled in the Planning View Model

---
+++ WorkoutHelper/Models/Plan.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WorkoutHelper.Models
+{
+    public class Plan
+    {
+        public string Weekday { get; set; }
+
+        public ObservableCollection<PlannedGroup> Groups { get; set; }
+
+        public bool Enabled { get; set; }
+    }
+}
---
+++ WorkoutHelper/Models/PlannedExercise.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WorkoutHelper.Models
+{
+    public class PlannedExercise
+    {
+        public string MuscleGroup { get; set; }
+
+        public int Difficulty { get; set; }
+
+        public IReadOnlyList<EquipmentCategory> EquipmentCategory { get; set; }
+    }
+}
