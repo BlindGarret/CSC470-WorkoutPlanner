23b56cf634c172df663504cc9c81d9720c8ebfbf
Brandon Widner <brandon_widner@yahoo.com>
2018-10-30 15:22:32 -0500

    Re-added previous comment, removed others.

--- WorkoutHelper/DesignViewModels/WeighInDesignViewModel.cs
+++ WorkoutHelper/DesignViewModels/WeighInDesignViewModel.cs
@@ -3,20 +3,13 @@ using Prism.Commands;
 using Prism.Mvvm;
 using WorkoutHelper.Models;
 
-//This is not a class which really ships with the product. This is basically a stub for building UIs.
-
-//What you do is stub in a the properties and commands from your real view model, with static values.
-// This allows you to use the visual preview of your form in VS with values, and gives you intellisense when typing out WPF
-
-//No need to DOC comment these members.
-
 namespace WorkoutHelper.DesignViewModels
 {
     public class ShellDesignViewModel : BindableBase
     {
         #region Properties
 
-        public double Weight { get; set; } = 200.0;
+        public double Weight { get; set; } = 200.0; //Somehow prepopulate with previous weight?
 
         #endregion
 
