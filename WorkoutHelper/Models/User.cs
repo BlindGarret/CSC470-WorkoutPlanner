017e877e309437d59b733835b20cf0d6225da28a
Lucas m. Roe <lroe@eandrcleaners.local>
2018-10-30 19:35:36 +0000

    Filled in the Select User View model

---
+++ WorkoutHelper/Models/User.cs
@@ -0,0 +1,15 @@
+﻿namespace WorkoutHelper.Models
+{
+    public class User
+    {
+        public string FirstName { get; set; }
+
+        public string LastName { get; set; }
+
+        public double Height { get; set; }
+
+        public double Weight { get; set; }
+
+        public string Avatar { get; set; }
+    }
+}
