309cd87607bef7bf5bffc78be30886071753e576
Lucas m. Roe <lroe2930@gmail.com>
2018-11-14 19:10:32 +0000

    rearranged page

--- WorkoutHelper/DesignViewModels/PlanningDesignViewmodel.cs
+++ WorkoutHelper/DesignViewModels/PlanningDesignViewmodel.cs
@@ -1,4 +1,6 @@
-﻿using System.Collections.ObjectModel;
+﻿using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
 using Prism.Commands;
 using Prism.Mvvm;
 using WorkoutHelper.Models;
@@ -26,5 +28,107 @@ namespace WorkoutHelper.DesignViewModels
         public DelegateCommand SaveCommand { get; set; }
 
         #endregion
+
+        public PlanningDesignViewModel()
+        {
+            Plans = MockPlan();
+        }
+
+        private static ObservableCollection<ObservablePlannedWeekday> MockPlan()
+        {
+            var exerciseSetOne = new[]
+            {
+                new PlannedExercise
+                {
+                    AllowFreeWeights = false,
+                    AllowMachines = true,
+                    Difficulty = 10,
+                    MuscleGroup = "A Muscle Group"
+                },
+                new PlannedExercise
+                {
+                    AllowFreeWeights = true,
+                    AllowMachines = true,
+                    Difficulty = 2,
+                    MuscleGroup = "A Muscle Group"
+                }
+            };
+
+            var exerciseSetTwo = new[]
+            {
+                new PlannedExercise
+                {
+                    AllowFreeWeights = true,
+                    AllowMachines = false,
+                    Difficulty = 3,
+                    MuscleGroup = "Another Muscle Group"
+                }
+            };
+
+            var groups = new[]
+            {
+                new PlannedGroup()
+                {
+                    DayOfWeek = "Sunday",
+                    Exercises = new List<PlannedExercise>(exerciseSetTwo),
+                    Order = 1,
+                    Id = 1
+                },
+                new PlannedGroup()
+                {
+                    DayOfWeek = "Sunday",
+                    Exercises = new List<PlannedExercise>(exerciseSetTwo),
+                    Order = 2,
+                    Id = 2
+                }
+            };
+
+            var weekDays = new[]
+            {
+                new PlannedWeekday
+                {
+                    Enabled = true,
+                    Groups = new List<PlannedGroup>(groups),
+                    Name = "Sunday"
+                },
+                new PlannedWeekday
+                {
+                    Enabled = true,
+                    Groups = new List<PlannedGroup>(groups),
+                    Name = "Monday"
+                },
+                new PlannedWeekday
+                {
+                    Enabled = true,
+                    Groups = new List<PlannedGroup>(groups),
+                    Name = "Tuesday"
+                },
+                new PlannedWeekday
+                {
+                    Enabled = true,
+                    Groups = new List<PlannedGroup>(groups),
+                    Name = "Wednesday"
+                },
+                new PlannedWeekday
+                {
+                    Enabled = true,
+                    Groups = new List<PlannedGroup>(groups),
+                    Name = "Thursday"
+                },
+                new PlannedWeekday
+                {
+                    Enabled = true,
+                    Groups = new List<PlannedGroup>(groups),
+                    Name = "Friday"
+                },
+                new PlannedWeekday
+                {
+                    Enabled = true,
+                    Groups = new List<PlannedGroup>(groups),
+                    Name = "Sat"
+                },
+            };
+            return new ObservableCollection<ObservablePlannedWeekday>(weekDays.Select(x => new ObservablePlannedWeekday(x)));
+        }
     }
 }
